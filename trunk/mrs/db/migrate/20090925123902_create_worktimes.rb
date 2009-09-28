2b42a9a6772bdf7fd3772a8899edd4b3667b9b23
wkoziej <wkoziej@b9c3e716-a6e9-11de-9ca8-173d21a9ba08>
2009-09-28 21:29:54 +0000

    Zmiana modelu na bardziej przystający (podział since, until) -> start_date, since, end_date, until

--- trunk/mrs/db/migrate/20090925123902_create_worktimes.rb
+++ trunk/mrs/db/migrate/20090925123902_create_worktimes.rb
@@ -1,12 +1,14 @@
 class CreateWorktimes < ActiveRecord::Migration
   def self.up
     create_table :worktimes do |t|
-      t.datetime :since,                       :null => false
-      t.datetime :until,                       :null => false
+      t.date :start_date,                      :null => false
+      t.date :end_date,                        :null => false
+      t.time :since,                        :null => false # in minutes
+      t.time :until,                        :null => false # in minutes
       t.integer :doctor_id,                    :null => false
       t.integer :place_id,                     :null => false
       t.integer  :day_of_week,                 :null => false 
-      t.integer  :repetition,                   :null => false
+      t.integer  :repetition,                  :null => false
       t.timestamps
     end
   end
