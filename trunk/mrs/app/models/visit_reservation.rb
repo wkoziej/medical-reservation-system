a1d40a47bf9f3a8d1bfb7aef22cc9cdb648e19d8
wkoziej <wkoziej@b9c3e716-a6e9-11de-9ca8-173d21a9ba08>
2009-10-12 20:24:29 +0000

    overlaping in visit_reservation

--- trunk/mrs/app/models/visit_reservation.rb
+++ trunk/mrs/app/models/visit_reservation.rb
@@ -10,8 +10,22 @@ class VisitReservation < ActiveRecord::Base
   state :CLOSED
   
   include Period::Util
+  include Period::Check
 
   validates_presence_of :until, :since, :patient, :doctor
+  
+  def validate
+    if not period_valid?
+      errors.add("visit_reservation_time", "Visit reservation period not valid")
+    end
+    if period_overlap?("doctor_id = :doctor_id", {:doctor_id => self.doctor_id})
+      errors.add("visit_reservation_time", "Visit reservation time overlap")
+    end
+    if period_overlap?("patient_id = :patient_id", {:patient_id => self.patient_id})
+      errors.add("visit_reservation_time", "Visit reservation time overlap")
+    end
+
+  end
   
   def short_info
     patient.name + " " + since.strftime("%Y-%m-%d %H:%M")
