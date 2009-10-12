bb466429deee18fa5fea02be680b56add6cdc8b3
wkoziej <wkoziej@b9c3e716-a6e9-11de-9ca8-173d21a9ba08>
2009-10-12 20:25:04 +0000

    cleanup

--- trunk/mrs/app/models/visit_reservation.rb
+++ trunk/mrs/app/models/visit_reservation.rb
@@ -31,12 +31,6 @@ class VisitReservation < ActiveRecord::Base
     patient.name + " " + since.strftime("%Y-%m-%d %H:%M")
   end
 
-  # def self.reservations_at_day (doctor_id, day)
-  #   VisitReservation.find(:all, :conditions => [ " status != :canceled and doctor_id = :doctor_id and ( :day between since and until or (since between :day and :next_day) ) " ,
-  #                                                { :canceled => "CANCELED", :doctor_id => doctor_id, :day => day, :next_day => (day + 1.days) }
-  #                                              ])
-  # end
-
   def reservations_at_day (doctor_id, day)
     rows_at_day(day, "doctor_id = :doctor_id and status != :canceled", {:doctor_id => doctor_id, :canceled => "CANCELED"})
   end
