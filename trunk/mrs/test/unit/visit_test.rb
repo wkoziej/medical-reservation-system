b7030f0678e3f19d2134b429ba989b782497b99f
wkoziej <wkoziej@b9c3e716-a6e9-11de-9ca8-173d21a9ba08>
2009-10-17 21:02:16 +0000

    Test update

--- trunk/mrs/test/unit/visit_test.rb
+++ trunk/mrs/test/unit/visit_test.rb
@@ -4,13 +4,24 @@ require File.dirname(__FILE__) + '/../test_helper'
 class VisitTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   test "overlap" do
+    ## Has to be worktime
+    w = Worktime.new 
+    w.start_date = '2000-01-01'
+    w.end_date = '2000-01-01'
+    w.since = '2000-01-01 00:00:00'
+    w.until = '2000-01-01 23:00:00' 
+    w.repetition = 1
+    w.doctor_id = 2
+    w.place_id = 2
+    assert w.save 
+
     a = Visit.new 
     a.since = '2000-01-01 08:00:00'
     a.until = a.since + 15.minutes
     a.patient_id = 1  
     a.doctor_id = 2
     
-    a.save
+    assert a.save
 
     b = Visit.new
     b.patient_id = 1  
@@ -37,10 +48,22 @@ class VisitTest < ActiveSupport::TestCase
     
     assert (not b.save)
 
-    b.since = a.since + 15.minutes
+    b.since = a.until
     b.until = a.until + 15.minutes
     
+
     assert b.save
+
+    ## Has to be worktime
+    w = Worktime.new 
+    w.start_date = '2000-01-01'
+    w.end_date = '2000-01-01'
+    w.since = '2000-01-01 00:00:00'
+    w.until = '2000-01-01 23:00:00' 
+    w.repetition = 1
+    w.doctor_id = 1 ## 2
+    w.place_id = 2
+    assert w.save 
 
     b = Visit.new
     b.patient_id = 2 # 1
