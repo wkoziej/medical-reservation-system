3de6470c8c9abfd592ebc1006249e6478cf2d47f
wkoziej <wkoziej@b9c3e716-a6e9-11de-9ca8-173d21a9ba08>
2009-10-18 21:52:34 +0000

    Start of translations

--- mrs/app/models/absence.rb
+++ mrs/app/models/absence.rb
@@ -3,13 +3,16 @@ class Absence < ActiveRecord::Base
 
   include Period::Check
   include Period::Util
+
+  validates_presence_of :couse
   
   def validate
     if not period_valid?
-      errors.add("absence_time", "Absence period not valid")
+      errors.add(:since, :absence_period_not_valid)
+      errors.add(:until, :absence_period_not_valid)
     end
     if period_overlap?("doctor_id = :doctor_id", {:doctor_id => self.doctor_id})
-      errors.add("absence_time", "Absence time overlap")
+      errors.add(t(:absence_time), t(:absence_time_overlap))
     end
   end
   
